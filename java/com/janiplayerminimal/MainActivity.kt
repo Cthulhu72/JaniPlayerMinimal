bb27f9577b676b3471de202f992f6e0ae7846ae0
Cthulhu72 <18x72y@gmail.com>
2026-08-04 12:21:25 +0200

    Refactor MainActivity to use layout resource directly
    Removed View Binding and updated content view.

--- java/com/janiplayerminimal/MainActivity.kt
+++ java/com/janiplayerminimal/MainActivity.kt
@@ -2,17 +2,10 @@ package com.janiplayerminimal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.janiplayer.minimal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.textView.text = "Jani Player Minimal működik!"
+        setContentView(R.layout.activity_main)
     }
 }
