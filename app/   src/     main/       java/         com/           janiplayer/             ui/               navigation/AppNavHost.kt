634a368c971a959c3c5c68f418e5841e58a7fffa
Cthulhu72 <18x72y@gmail.com>
2026-08-05 17:23:47 +0200

    Add AppNavHost for navigation in the app

---
+++ app/   src/     main/       java/         com/           janiplayer/             ui/               navigation/AppNavHost.kt
@@ -0,0 +1,25 @@
+package com.janiplayer.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.janiplayer.ui.player.PlayerScreen
+import com.janiplayer.ui.settings.SettingsScreen
+import com.janiplayer.ui.settings.AudioEffectsSettingsScreen
+
+@Composable
+fun AppNavHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "player"
+    ) {
+        composable("player") {
+            PlayerScreen()
+        }
+
+        composable("settings") {
+            SettingsScreen(
+                onAudioEffectsClick = { navController.navigate("audio_effects_settings") },
+                onThemeClick = { /* később: navController.navigate("theme_settings") */ },
+                onAppInfoClick = { /* később: navController.navigate("app_info") */ }
