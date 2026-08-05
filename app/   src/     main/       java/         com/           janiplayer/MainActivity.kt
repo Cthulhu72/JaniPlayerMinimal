9bbdecd82e8b0a9d5c419f7b43628e897f448f30
Cthulhu72 <18x72y@gmail.com>
2026-08-05 17:31:21 +0200

    Add MainActivity with navigation setup
    Implement MainActivity with navigation and theming.

--- app/   src/     main/       java/         com/           janiplayer/MainActivity.kt
+++ app/   src/     main/       java/         com/           janiplayer/MainActivity.kt
@@ -1,1 +1,28 @@
+package com.janiplayerminimal
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.janiplayer.ui.navigation.AppNavHost
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JaniPlayerApp()
+        }
+    }
+}
+
+@Composable
+fun JaniPlayerApp() {
+    val navController = rememberNavController()
+    Surface(color = MaterialTheme.colorScheme.background, modifier = Modifier) {
+        AppNavHost(navController = navController)
+    }
+}
