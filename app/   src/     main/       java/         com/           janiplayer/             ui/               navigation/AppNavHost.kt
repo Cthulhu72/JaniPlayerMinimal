9a432a547e53388c467e83a445fcc1af43c43060
Cthulhu72 <18x72y@gmail.com>
2026-08-05 17:42:37 +0200

    Update AppNavHost.kt

--- app/   src/     main/       java/         com/           janiplayer/             ui/               navigation/AppNavHost.kt
+++ app/   src/     main/       java/         com/           janiplayer/             ui/               navigation/AppNavHost.kt
@@ -7,15 +7,19 @@ import androidx.navigation.compose.composable
 import com.janiplayer.ui.player.PlayerScreen
 import com.janiplayer.ui.settings.SettingsScreen
 import com.janiplayer.ui.settings.AudioEffectsSettingsScreen
+import androidx.media3.exoplayer.ExoPlayer
 
 @Composable
-fun AppNavHost(navController: NavHostController) {
+fun AppNavHost(
+    navController: NavHostController,
+    player: ExoPlayer
+) {
     NavHost(
         navController = navController,
         startDestination = "player"
     ) {
         composable("player") {
-            PlayerScreen()
+            PlayerScreen(player = player)
         }
 
         composable("settings") {
@@ -23,3 +27,13 @@ fun AppNavHost(navController: NavHostController) {
                 onAudioEffectsClick = { navController.navigate("audio_effects_settings") },
                 onThemeClick = { /* később: navController.navigate("theme_settings") */ },
                 onAppInfoClick = { /* később: navController.navigate("app_info") */ }
+            )
+        }
+
+        composable("audio_effects_settings") {
+            AudioEffectsSettingsScreen(
+                onBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
