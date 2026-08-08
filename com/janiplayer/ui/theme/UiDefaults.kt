e204ca59ad5e5d6f964073974fe39f4f5a704659
Cthulhu72 <18x72y@gmail.com>
2026-08-08 17:15:20 +0200

    Add UiDefaults object with spacing values

---
+++ com/janiplayer/ui/theme/UiDefaults.kt
@@ -0,0 +1,10 @@
+package com.janiplayer.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object UiDefaults {
+    val screenPadding = 16.dp
+    val itemPadding = 12.dp
+    val sectionSpacing = 24.dp
+    val smallSpacing = 8.dp
+}
