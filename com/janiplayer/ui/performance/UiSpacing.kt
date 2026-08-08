d222985f12e0672bdfca0fc93f857d17e7649b43
Cthulhu72 <18x72y@gmail.com>
2026-08-08 21:12:18 +0200

    Add UiSpacing object for consistent spacing values

---
+++ com/janiplayer/ui/performance/UiSpacing.kt
@@ -0,0 +1,10 @@
+package com.janiplayer.ui.performance
+
+import androidx.compose.ui.unit.dp
+
+object UiSpacing {
+    val screen = 16.dp
+    val item = 12.dp
+    val section = 24.dp
+    val small = 8.dp
+}
