13b29773cfdea77318e31cac0d149494a2251a45
Cthulhu72 <18x72y@gmail.com>
2026-08-08 18:38:23 +0200

    Modify VideoPlayerScreen to include media path parameter
    Updated VideoPlayerScreen to accept a media path as a parameter and set the media item accordingly.

--- com/janiplayer/video/ui/VideoPlayerScreen.kt
+++ com/janiplayer/video/ui/VideoPlayerScreen.kt
@@ -1,20 +1,32 @@
 package com.janiplayer.video.ui
 
-import android.view.ViewGroup
+import android.content.Context
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.janiplayer.video.player.OptimizedPlayer
+import com.janiplayer.video.player.VideoSource
+import com.janiplayer.video.player.VideoPlayerController
 import com.janiplayer.video.viewmodel.PositionState
 
 @Composable
-fun VideoPlayerScreen(context: android.content.Context) {
-
+fun VideoPlayerScreen(
+    context: Context,
+    path: String
+) {
     val player = remember { OptimizedPlayer.create(context) }
+    val controller = remember { VideoPlayerController(player) }
     val positionState = remember { PositionState(player) }
 
     PlayerLifecycleFix(player)
+
+    LaunchedEffect(path) {
+        player.setMediaItem(VideoSource.fromPath(path))
+        player.prepare()
+        controller.play()
+    }
 
     Column(Modifier.fillMaxSize()) {
 
